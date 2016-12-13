f68d6cfae36f4628cdc942b9602c48e184573b37
Hampus <hampus.kensing@prepaird.com>
2016-12-13 21:59:01 +0100

    Compile to es6 and use Promise

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import LayoutFileReader = require('./layout-file-reader');
 
-console.log('hoho');
 let layoutReader = new LayoutFileReader();
-layoutReader.readLayoutFile('../layouts/cklb-default-layout.json');
+let promise = layoutReader.readLayoutFile('../layouts/cklb-default-layout.json');
+promise.then(console.dir);
--- src/layout-file-reader.ts
+++ src/layout-file-reader.ts
@@ -2,15 +2,17 @@ import fs = require('fs');
 
 class LayoutFileReader {
 
-  readLayoutFile(filePath: string) {
-    let onFileRead = (error: Error, rawLayout: string) => {
-      if (error) {
-        throw error;
-      }
-      let layout = JSON.parse(rawLayout);
-      console.dir(layout);
-    };
-    fs.readFile(filePath, 'utf8', onFileRead);
+  readLayoutFile(filePath: string): Promise<any> {
+    return new Promise((resolve: (result: any) => void, reject: (error: Error) => void) => {
+      let onFileRead = (error: Error, rawLayout: string) => {
+        if (error) {
+          throw error;
+        }
+        let layout = JSON.parse(rawLayout);
+        resolve(layout);
+      };
+      fs.readFile(filePath, 'utf8', onFileRead);
+    });
   }
 
 }
