81cc5f346bf5b1c9e4e34a974c913f4b8bbda5e3
Hampus <hampus.kensing@prepaird.com>
2016-12-13 23:06:55 +0100

    Calculate row width

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import LayoutFileReader = require('./layout-file-reader');
 
 let layoutReader = new LayoutFileReader();
 let promise = layoutReader.readLayoutFile('../layouts/cklb-default-layout.json');
-promise.then(console.dir);
+promise.then((keyboard: any) => {
+  keyboard.layout.forEach((keys: { legend: string, units?: number }[]) => {
+    console.log(keys.map(key => key.units || 1).reduce((a, b) => a + b) * keyboard.meta.unitSize);
+  });
+});
