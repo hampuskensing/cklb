80923249d9017205a272f89ad42c1fc0bfa118ff
Hampus <hampus.kensing@prepaird.com>
2016-12-15 22:55:05 +0100

    Use xmlbuilder

--- src/layout-builder.ts
+++ src/layout-builder.ts
@@ -1,29 +1,38 @@
+import builder = require('xmlbuilder');
+import XMLElementOrXMLNode = require('xmlbuilder');
+
 class LayoutBuilder {
 
   readonly PIXEL_TO_MM_RATIO: number = 72/19.05;
 
   createLayout(layout: { legend: string, units?: number }[][], switchSpecification: any, unitSize: number) {
-    let svgString =  `<svg xmlns="http://www.w3.org/2000/svg" width="260mm" height="80mm">\n`;
+    let svg = builder.create('svg')
+      .att('xmlns', 'http://www.w3.org/2000/svg')
+      .att('width', '260mm')
+      .att('height', '80mm');
 
     for (let i = 0; i < layout.length; i++) {
       let row = layout[i];
-      svgString += `  <g transform="translate(0, ${(unitSize*i) * this.PIXEL_TO_MM_RATIO})">\n`;
+      let switchRowEle = (<any>builder).begin()
+        .ele('g', { transform: `translate(0, ${(unitSize*i) * this.PIXEL_TO_MM_RATIO})` });
       let currentTopX = 0;
       for (let j = 0; j < row.length; j++) {
         let key = row[j];
-        svgString += `    <g name="${key.legend}" transform="translate(${currentTopX}, 0)">\n`;
-        svgString += this.createSwitchHoles(key, unitSize, switchSpecification);
-        svgString += `    </g>\n`;
+        let switchHolesEle = this.createSwitch(key, currentTopX, unitSize, switchSpecification);
+
+        (<any>switchRowEle).importDocument(switchHolesEle);
         currentTopX += (key.units || 1) * unitSize * this.PIXEL_TO_MM_RATIO;
       }
-      svgString += `  </g>\n`;
+      (<any>svg).importDocument(switchRowEle);
     }
-    svgString += '</svg>';
-    return svgString;
+    svg.end({ pretty: true });
+    return svg.toString();
   }
 
-  createSwitchHoles(key: { units?: number }, unitSize: number, switchJson: any): string {
-    let result = '';
+  createSwitch(key: { units?: number, legend: string }, currentTopX: number, unitSize: number, switchJson: any): any {
+    let switchHolesEle = (<any>builder).begin().ele('g')
+      .att('legend', key.legend)
+      .att('transform', `translate(${currentTopX}, 0)`);
     let holeTypes = switchJson.switchHoles;
     let switchWidth = switchJson.width;
     let switchHeight = switchJson.height;
@@ -32,12 +41,15 @@ class LayoutBuilder {
       for (let j = 0; j < holeType.holes.length; j++) {
         let unit = key.units || 1;
         let hole = holeType.holes[j];
-        result += `      <circle r="${holeType.diameter/2}mm"` +
-          ` cx="${((unit * unitSize) / 2) - (switchWidth/2) + hole.x}mm"` +
-          ` cy="${(unitSize / 2) - (switchHeight/2) + hole.y}mm" fill="#bbbbbb"/>\n`
+        let switchHoleEle = builder.create('circle')
+          .att('r', `${holeType.diameter/2}mm`)
+          .att('cx', `${((unit * unitSize) / 2) - (switchWidth/2) + hole.x}mm`)
+          .att('cy', `${(unitSize / 2) - (switchHeight/2) + hole.y}mm`)
+          .att('fill', '#bbbbbb')
+        switchHolesEle.importDocument(switchHoleEle);
       }
     }
-    return result;
+    return switchHolesEle;
   }
 
 }
